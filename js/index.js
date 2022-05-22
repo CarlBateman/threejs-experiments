b41714393f78a0bc14e7d8ccedc152d781ed6e7a
CarlBateman <CarlBateman@hotmailcom>
2022-05-22 18:37:39 +0100

    Compare points against frustum rather than plane Changed winding for near plane Compare vertex positions to frustum

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 10;
-  camera.position.x = 5;
+  //camera.position.x = 5;
   camera.lookAt(0, 0, 0);
 
   const helper = new SelectionHelper(renderer, 'selectBox');
@@ -39,61 +39,68 @@
   const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 
   let models = [];
-  for (var i = 0; i < 10; i++) {
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    const cube = new THREE.Mesh(boxGeometry, material);
-    //cube.name = i;
+  if (true) {
+    for (var i = 0; i < 10; i++) {
+      const material = new THREE.MeshPhongMaterial({ color: 0xffffff/*, wireframe: true*/ });
+      const cube = new THREE.Mesh(boxGeometry, material);
+      //cube.name = i;
 
-    cube.position.set(-5 + Math.random() * 10, -5 + Math.random() * 10, -5 + Math.random() * 10);
-    scene.add(cube);
-    models.push(cube);
+      //cube.geometry.computeBoundingBox();
+      cube.position.set(-15 + Math.random() * 30, Math.random() * 10, -15 + Math.random() * 30);
+      cube.rotation.set(-5 + Math.random() * 10, -5 + Math.random() * 10, -5 + Math.random() * 10);
+      cube.scale.set(0.1 + Math.random() * 5, 0.1 + Math.random() * 5, 0.1 + Math.random() * 5);
+
+      scene.add(cube);
+      models.push(cube);
+
+      //const box = new THREE.BoxHelper(cube, 0xffff00);
+      //scene.add(box);
+    }
+  } else {
+    for (var i = 0; i < 1; i++) {
+      const material = new THREE.MeshPhongMaterial({ color: 0xffffff/*, wireframe: true*/ });
+      const cube = new THREE.Mesh(boxGeometry, material);
+
+      //cube.position.set(-5 + i * 2, 0, 0);
+      //cube.rotation.set(Math.PI / 10, 0, 0);
+      //cube.scale.set(1, 5, 1);
+      scene.add(cube);
+      models.push(cube);
+    }
   }
 
-  const sphereGeometry = new THREE.SphereGeometry();
-  for (var i = 0; i < 10; i++) {
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    const cube = new THREE.Mesh(sphereGeometry, material);
-    //cube.name = i;
+  //const sphereGeometry = new THREE.SphereGeometry();
+  //for (var i = 0; i < 10; i++) {
+  //  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  //  const cube = new THREE.Mesh(sphereGeometry, material);
+  //  //cube.name = i;
 
-    cube.position.set(-5 + Math.random() * 10, -5 + Math.random() * 5, -5 + Math.random() * 10);
-    scene.add(cube);
-    models.push(cube);
-  }
+  //  cube.position.set(-5 + Math.random() * 15, -5 + Math.random() * 15, -5 + Math.random() * 5);
+  //  cube.rotation.set(-5 + Math.random() * 10, -5 + Math.random() * 10, -5 + Math.random() * 10);
+  //  scene.add(cube);
+  //  models.push(cube);
 
-  const cylinderGeometry = new THREE.CylinderGeometry();
-  for (var i = 0; i < 10; i++) {
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    const cube = new THREE.Mesh(cylinderGeometry, material);
-    //cube.name = i;
+  //  //const box = new THREE.BoxHelper(cube, 0xffff00);
+  //  //scene.add(box);
+  //}
 
-    cube.position.set(-5 + Math.random() * 10, -5 + Math.random() * 5, -5 + Math.random() * 10);
-    scene.add(cube);
-    models.push(cube);
-  }
+  //const cylinderGeometry = new THREE.CylinderGeometry();
+  //for (var i = 0; i < 10; i++) {
+  //  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  //  const cube = new THREE.Mesh(cylinderGeometry, material);
+  //  //cube.name = i;
+
+  //  cube.position.set(-5 + Math.random() * 15, -5 + Math.random() * 15, -5 + Math.random() * 5);
+  //  cube.rotation.set(-5 + Math.random() * 10, -5 + Math.random() * 10, -5 + Math.random() * 10);
+  //  scene.add(cube);
+  //  models.push(cube);
+
+  //  //const box = new THREE.BoxHelper(cube, 0xffff00);
+  //  //scene.add(box);
+  //}
 
 
-  const planeLft = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
-  //const helper0 = new THREE.PlaneHelper(plane0, 10, 0xffff00);
-  //helper0.layers.set(1);
-  //scene.add(helper0);
 
-  const planeRgt = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
-  //const helper1 = new THREE.PlaneHelper(plane1, 10, 0xffff00);
-  //helper1.layers.set(1);
-  //scene.add(helper1);
-
-  const planeBot = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
-  //const helper2 = new THREE.PlaneHelper(plane2, 10, 0xffff00);
-  //helper2.layers.set(1);
-  //scene.add(helper2);
-
-  const planeTop = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
-  //const helper3 = new THREE.PlaneHelper(plane3, 10, 0xffff00);
-  //helper3.layers.set(1);
-  //scene.add(helper3);
-
-  const planeFar = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
-  const planeNear = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
 
   const _frustum = new THREE.Frustum();
 
@@ -126,6 +133,7 @@
 
     if (startPt.distanceToSquared(endPt) < 25) {
       pickPoint(pointer);
+      //console.log(pointer);
     } else {
       const pointer2 = new THREE.Vector2();
       pointer2.x = ((startPt.x - rect.left) / rect.width) * 2 - 1;
@@ -135,6 +143,10 @@
   }
 
   function pickPoint(pointer) {
+    //planeNear = new THREE.Plane(new THREE.Vector3(1, 0, 0), -110);
+    _frustum.planes[0] = null;
+
+
     const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(pointer, camera);
     const intersects = raycaster.intersectObjects(scene.children);
@@ -144,14 +156,34 @@
     //}
 
     for (let i = 0; i < intersects.length; i++) {
-      if (intersects[i].object.name === "back") continue;
       intersects[i].object.material.color.set(0xff0000);
     }
   }
 
 
   function selectArea(pointer0, pointer1) {
-    //camera.layers.enable(1);
+    let planeLft = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
+    //const helper0 = new THREE.PlaneHelper(plane0, 10, 0xffff00);
+    //helper0.layers.set(1);
+    //scene.add(helper0);
+
+    let planeRgt = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
+    //const helper1 = new THREE.PlaneHelper(plane1, 10, 0xffff00);
+    //helper1.layers.set(1);
+    //scene.add(helper1);
+
+    let planeBot = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
+    //const helper2 = new THREE.PlaneHelper(plane2, 10, 0xffff00);
+    //helper2.layers.set(1);
+    //scene.add(helper2);
+
+    let planeTop = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
+    //const helper3 = new THREE.PlaneHelper(plane3, 10, 0xffff00);
+    //helper3.layers.set(1);
+    //scene.add(helper3);
+
+    let planeFar = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
+    let planeNear = new THREE.Plane(new THREE.Vector3(1, 0, 0), -11);
 
     let pointerBL = pointer0.clone();
     let pointerTR = pointer1.clone();
@@ -184,6 +216,20 @@
     planeFar.setFromCoplanarPoints(v1, v2, v3);
     //const helperFar = new THREE.PlaneHelper(planeFar, 1000, 0xffff00);
     //scene.add(helperFar);
+
+
+    // near plane
+    camera.getWorldDirection(fwd);
+    vToPlane = fwd.multiplyScalar(camera.near);
+
+    v1 = camera.position.clone().add(vToPlane);
+    v2 = v1.clone().add(cameraRightVector);
+    v3 = v1.clone().add(camera.up);
+
+    //let up = camera.position.clone().add(camera.up);
+    planeNear.setFromCoplanarPoints(v1, v3, v2);
+
+
 
 
 
@@ -219,10 +265,6 @@
     // formed from camera position, top right intersection point and camera right vector
     planeTop.setFromCoplanarPoints(camera.position, intersectsTR1, offset);
 
-    // near plane
-    let up = camera.position.clone().add(camera.up);
-    planeNear.setFromCoplanarPoints(camera.position, cameraRightVector, up);
-
     const planes = _frustum.planes;
 
     planes[0] = planeTop;
@@ -233,6 +275,13 @@
     planes[5] = planeFar;
 
     findIntersectedObjects();
+
+    //planeTop = null;
+    //planeRgt = null;
+    //planeBot = null;
+    //planeLft = null;
+    //planeNear = null;
+    //planeFar = null;
   }
 
   function findEnclosedObjects() {
@@ -240,27 +289,64 @@
   }
 
   function findIntersectedObjects() {
-    // find all objects between all planes
-    // 1. check position - definitely inside
-
+    // inside frustum? orientation of plane?
+    let found = false;
     for (let i = 0; i < models.length; i++) {
-      let distLft = planeLft.distanceToPoint(models[i].position);
-      let distRgt = planeRgt.distanceToPoint(models[i].position);
-      let distBot = planeBot.distanceToPoint(models[i].position);
-      let distTop = planeTop.distanceToPoint(models[i].position);
-      //if (dist0 * dist2 < 0 && dist1 * dist3 < 0)
-
-      //if (distBot > 0 && distTop < 0 && distLft < 0 && distRgt > 0)
-      if (distTop > 0 && distBot > 0 && distLft > 0 && distRgt > 0)
+      // quick selected
+      // centre is inside frustum
+      if (_frustum.containsPoint(models[i].position)) {
         models[i].material.color.set(0xff0000);
+        continue;
+      }
+
+      // quick rejections
+      // bounding sphere is wholly outside selection frustum 
+      //if (_frustum.intersectsObject(models[i]) === false) continue;
+
+      //if (models[i].geometry.boundingBox === null) {
+      //  models[i].geometry.computeBoundingBox();
+      //}
+
+      //let box = new THREE.Box3();
+      //box.copy(models[i].geometry.boundingBox).applyMatrix4(models[i].matrixWorld);
+
+      //// bounding box is wholly outside selection frustum 
+      //if (_frustum.intersectsBox(box) === false) {
+      //  //models[i].material.color.set(0xff0000);
+      //  continue;
+      //}
+
+      //box = models[i].clone();
+      //box.applyMatrix4(models[i].matrixWorld);
+      const vertices1 = models[i].geometry.getAttribute('position');
+      const vertices = models[i].geometry.getAttribute('position').clone().applyMatrix4(models[i].matrixWorld);
+
+      // any point is inside frustum
+      for (let j = 0; j < vertices.array.length; j += 3) {
+        let point = new THREE.Vector3(vertices.array[j], vertices.array[j+1], vertices.array[j+2]);
+        if (_frustum.containsPoint(point)) {
+          models[i].material.color.set(0xff0000);
+          j = vertices.array.length;
+          found = true;
+        }
+      }
+      if (found) continue;
+
+      //for (let i = 0; i < models.length; i++) {
+      //  let distLft = planeLft.distanceToPoint(models[i].position);
+      //  let distRgt = planeRgt.distanceToPoint(models[i].position);
+      //  let distBot = planeBot.distanceToPoint(models[i].position);
+      //  let distTop = planeTop.distanceToPoint(models[i].position);
+      //  //if (dist0 * dist2 < 0 && dist1 * dist3 < 0)
+
+      //  //if (distBot > 0 && distTop < 0 && distLft < 0 && distRgt > 0)
+      //  if (distTop > 0 && distBot > 0 && distLft > 0 && distRgt > 0)
+      //    models[i].material.color.set(0xff0000);
+      //}
+
     }
 
 
-    // any vertex is inside
-    // quick exclude? if bounded box, sphere is outside
-    // sphere, then box
-    // then points inside
-    // then intersections
 
     // For plane
     // .distanceToSphere
@@ -271,7 +357,7 @@
     // For frustum
     // .containsPoint
     // .intersectsBox
-    // .intersectsObjec
+    // .intersectsObject
     // .intersectsSphere
 
 
@@ -287,6 +373,7 @@
   let angle = 0;
   function animate() {
     //camera.position.x += .01;
+    //models[0].rotation.x += .01;
     //angle += .01;
     //sphere.position.z = Math.cos(angle) * 10;
     //sphere.position.x = Math.sin(angle) * 10;
